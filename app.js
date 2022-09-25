e9bc34b701c0062465eb73262b006de9923d6336
Duc Thong <thongtd@dipro.vn>
2022-09-25 08:34:45 +0700

    test port

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ var express = require("express");
 require("dotenv").config();
 var app = express();
 var dbConnect = require("./database/connection")
-const PORT = 3000;
+// const PORT = 3000;
 
 dbConnect();
 
 app.get("/", (req, res, next) => {
     res.send("Server is runningg")
 })
-app.listen(PORT, () => {
+app.listen(3000, () => {
     console.log("Starting Express Server");
 })
