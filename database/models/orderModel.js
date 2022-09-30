a5649819c5f1654cfddd7b1d411920edf0b1a35e
Soukthavilay <Soukthavilay2000@gmail.com>
2022-09-30 14:36:29 +0700

    add orderModel statusSchema and order_item

--- database/models/orderModel.js
+++ database/models/orderModel.js
@@ -1,1 +1,47 @@
-const v = 3 ;
+const mongoose = require('mongoose');
+const orderSchema = new mongoose.Schema({
+  status: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Status',
+  },
+  create_at: {
+    type: Date.now(),
+  },
+  complete_at: {
+    type: Date.now(),
+  },
+  order_item: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Order_item',
+  },
+  shipping_code: {
+    type: String,
+    required: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
+});
+const order_item = new mongoose.Schema({
+  amount: {
+    type: Number,
+    required:true,
+  },
+  product:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:'Product'
+  },
+});
+
+const status = new mongoose.Schema({
+    title:{
+        type:String,
+        required:true,
+    },
+});
+let Order = mongoose.model('Order', orderSchema);
+let Order_item = mongoose.model('Order_item', order_item);
+let Status = mongoose.model('Status', status);
+
+module.exports = { Order , Order_item , Status };
