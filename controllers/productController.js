44409a27c4de951a50812dffc6b2ca66e8d4f0a1
Duc Thong <thongtd@dipro.vn>
2022-10-03 16:32:48 +0700

    fix: refactor name

--- controllers/productController.js
+++ controllers/productController.js
@@ -3,19 +3,19 @@ const OrderItem = require('../database/models/orderItemModel');
 const Status = require('../database/models/statusModel');
 const Product = require('../database/models/productModel');
 
-exports.AddProduct= async (req, res)=>{
-    try {
-        const newProduct = new Product(req.body);
-        const save = await newProduct.save();
-        res.status(200).json(save);
-      } catch (err) {
-        res.status(500).json(err);
-      }
+exports.AddProduct = async (req, res) => {
+  try {
+    const newProduct = new Product(req.body);
+    const save = await newProduct.save();
+    res.status(200).json(save);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 };
-exports.getAProduct = async (req, res) => {
-  try{
+exports.getAllProduct = async (req, res) => {
+  try {
 
-  }catch(err){
+  } catch (err) {
     res.status(500).json(err);
   }
 }
