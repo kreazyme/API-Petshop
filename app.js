9e0cc1a6f182d8e8f8f0748303a288364027817e
Soukthavilay <Soukthavilay2000@gmail.com>
2022-10-04 17:44:29 +0700

    Feat/Assets Controller

--- app.js
+++ app.js
@@ -1,34 +1,36 @@
-var express = require("express");
-require("dotenv").config();
-var bodyParser = require("body-parser")
+var express = require('express');
+require('dotenv').config();
+var bodyParser = require('body-parser');
 
 var app = express();
-var dbConnect = require("./database/connection");
+var dbConnect = require('./database/connection');
 var userRouter = require('./routes/userRouter');
-var orderRoute = require("./routes/orderRouter");
-var SttRoute= require('./routes/statusRouter');
-const productRoute= require('./routes/productRouter');
+var orderRoute = require('./routes/orderRouter');
+var SttRoute = require('./routes/statusRouter');
+const productRoute = require('./routes/productRouter');
 const orderItemRoute = require('./routes/orderItem');
 const categoryRoute = require('./routes/categoryRouter');
 const typeRoute = require('./routes/typeRouter');
-const verifyToken = require("./middlewares/authJWT");
+const asset = require('./routes/assetsRouter');
+const verifyToken = require('./middlewares/authJWT');
 
 dbConnect();
-app.use("/auth", userRouter);
-app.use('/order',orderRoute);
+app.use('/auth', userRouter);
+app.use('/order', orderRoute);
 app.use('/status', SttRoute);
-app.use('/orderItem',orderItemRoute);
-app.use('/product',productRoute);
+app.use('/orderItem', orderItemRoute);
+app.use('/product', productRoute);
 app.use('/category', categoryRoute);
-app.use('/type',typeRoute);
-app.use(express.json())
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use('/type', typeRoute);
+app.use('/assets', asset);
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get("/", (req, res, next) => {
-    console.log(res);
-    res.send("Server is runningg")
-})
+app.get('/', (req, res, next) => {
+  console.log(res);
+  res.send('Server is runningg');
+});
 
 app.listen(process.env.PORT, () => {
-    console.log("Starting Express Server");
-})
+  console.log('Starting Express Server');
+});
