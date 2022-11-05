32bc9db8263ee839ee5c95c2938ac8a67479416a
Soukthavilay <Soukthavilay2000@gmail.com>
2022-11-05 21:55:20 +0700

    Create Type

--- routes/typeRouter.js
+++ routes/typeRouter.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const typeCtrl = require('../controllers/typeCtrl');
 const auth = require('../middleware/auth');
 const authAdmin = require('../middleware/authAdmin');
-
+// admin add type moi dc nha con cho
 router
   .route('/type')
   .get(typeCtrl.getTypes)
