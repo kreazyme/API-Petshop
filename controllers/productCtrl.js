9b67a84dbdb68b65bacd7ba7186aed610cefaa5a
Duc Thong <thongtd@dipro.vn>
2022-11-06 10:53:53 +0700

    feat: api search product

--- controllers/productCtrl.js
+++ controllers/productCtrl.js
@@ -3,40 +3,40 @@ const Products = require('../models/productModel')
 // Filter, sorting and paginating
 
 class APIfeatures {
-    constructor(query, queryString){
+    constructor(query, queryString) {
         this.query = query;
         this.queryString = queryString;
     }
-    filtering(){
-       const queryObj = {...this.queryString} //queryString = req.query
+    filtering() {
+        const queryObj = { ...this.queryString } //queryString = req.query
 
-       const excludedFields = ['page', 'sort', 'limit']
-       excludedFields.forEach(el => delete(queryObj[el]))
-       
-       let queryStr = JSON.stringify(queryObj)
-       queryStr = queryStr.replace(/\b(gte|gt|lt|lte|regex)\b/g, match => '$' + match)
+        const excludedFields = ['page', 'sort', 'limit']
+        excludedFields.forEach(el => delete (queryObj[el]))
 
-    //    gte = greater than or equal = lớn hơn hoặc bằng
-    //    lte = lesser than or equal = nhỏ hơn hoặc bằng
-    //    lt = lesser than = ít hơn
-    //    gt = greater than = lớn hơn 
-    // regex = search to nung sue 
-       this.query.find(JSON.parse(queryStr))
-       return this;
+        let queryStr = JSON.stringify(queryObj)
+        queryStr = queryStr.replace(/\b(gte|gt|lt|lte|regex)\b/g, match => '$' + match)
+
+        //    gte = greater than or equal = lớn hơn hoặc bằng
+        //    lte = lesser than or equal = nhỏ hơn hoặc bằng
+        //    lt = lesser than = ít hơn
+        //    gt = greater than = lớn hơn 
+        // regex = search to nung sue 
+        this.query.find(JSON.parse(queryStr))
+        return this;
     }
 
-    sorting(){
-        if(this.queryString.sort){
+    sorting() {
+        if (this.queryString.sort) {
             const sortBy = this.queryString.sort.split(',').join(' ')
             this.query = this.query.sort(sortBy)
-        }else{
+        } else {
             this.query = this.query.sort('-createdAt')
         }
 
         return this;
     }
 
-    paginating(){
+    paginating() {
         const page = this.queryString.page * 1 || 1
         const limit = this.queryString.limit * 1 || 9
         const skip = (page - 1) * limit;
@@ -46,10 +46,10 @@ class APIfeatures {
 }
 
 const productCtrl = {
-    getProducts: async(req, res) =>{
+    getProducts: async (req, res) => {
         try {
             const features = new APIfeatures(Products.find(), req.query)
-            .filtering().sorting().paginating()
+                .filtering().sorting().paginating()
 
             const products = await features.query
 
@@ -58,51 +58,61 @@ const productCtrl = {
                 result: products.length,
                 products: products
             })
-            
+
         } catch (err) {
-            return res.status(500).json({msg: err.message})
+            return res.status(500).json({ msg: err.message })
         }
     },
-    createProduct: async(req, res) =>{
+    createProduct: async (req, res) => {
         try {
-            const {product_id,type, title, description, amount, images, category,price} = req.body;
-            if(!images) return res.status(400).json({msg: "Không có hình ảnh tải lên"})
+            const { product_id, type, title, description, amount, images, category, price } = req.body;
+            if (!images) return res.status(400).json({ msg: "Không có hình ảnh tải lên" })
 
-            const product = await Products.findOne({product_id})
-            if(product)
-                return res.status(400).json({msg: "Sản phẩm này đã tồn tại."})
+            const product = await Products.findOne({ product_id })
+            if (product)
+                return res.status(400).json({ msg: "Sản phẩm này đã tồn tại." })
 
             const newProduct = new Products({
-                product_id,type, title:title.toLowerCase(), description, amount, images, category,price
+                product_id, type, title: title.toLowerCase(), description, amount, images, category, price
             })
 
             await newProduct.save()
-            res.json({msg: "Đã tạo ra một sản phẩm",newProduct})
+            res.json({ msg: "Đã tạo ra một sản phẩm", newProduct })
 
         } catch (err) {
-            return res.status(500).json({msg: err.message})
+            return res.status(500).json({ msg: err.message })
         }
     },
-    deleteProduct: async(req, res) =>{
+    deleteProduct: async (req, res) => {
         try {
             await Products.findByIdAndDelete(req.params.id)
-            res.json({msg: "Đã xóa một sản phẩm"})
+            res.json({ msg: "Đã xóa một sản phẩm" })
         } catch (err) {
-            return res.status(500).json({msg: err.message})
+            return res.status(500).json({ msg: err.message })
         }
     },
-    updateProduct: async(req, res) =>{
+    updateProduct: async (req, res) => {
         try {
-            const {type, title, description, amount, images, category,price} = req.body;
-            if(!images) return res.status(400).json({msg: "Không có hình ảnh tải lên"})
+            const { type, title, description, amount, images, category, price } = req.body;
+            if (!images) return res.status(400).json({ msg: "Không có hình ảnh tải lên" })
 
-            await Products.findOneAndUpdate({_id: req.params.id}, {
-                type, title:title.toLowerCase(), description, amount, images, category,price
+            await Products.findOneAndUpdate({ _id: req.params.id }, {
+                type, title: title.toLowerCase(), description, amount, images, category, price
             })
 
-            res.json({msg: "Đã cập nhật một sản phẩm"})
+            res.json({ msg: "Đã cập nhật một sản phẩm" })
         } catch (err) {
-            return res.status(500).json({msg: err.message})
+            return res.status(500).json({ msg: err.message })
+        }
+    },
+    searchProduct: async (req, res) => {
+        try {
+            const { searchToken } = req.body
+            const products = await Products.find({ title: { "$regex": searchToken, "$options": "i" } });
+            res.send(JSON.stringify(products));
+        }
+        catch (error) {
+            res.send(JSON.stringify(error))
         }
     }
 }
