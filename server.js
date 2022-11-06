ba8f4e12f9256dd6902575ea8d884b52caf19caa
Soukthavilay <Soukthavilay2000@gmail.com>
2022-11-06 08:30:29 +0700

    Add assets

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(fileUpload({
 app.use('/user', require('./routes/userRouter'))
 app.use('/api', require('./routes/categoryRouter'))
 app.use('/api', require('./routes/typeRouter'))
-app.use('/api', require('./routes/upload'))
+app.use('/api', require('./routes/imgRouter'))
 app.use('/api', require('./routes/productRouter'))
 app.use('/api', require('./routes/paymentRouter'))
 
