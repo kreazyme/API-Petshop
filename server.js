0de5aa723917a818cf49bbdabafe2ceacf1d305a
Duc Thong <thongtd@dipro.vn>
2022-11-26 09:07:53 +0700

    fix server.js

--- server.js
+++ server.js
@@ -51,6 +51,15 @@ mongoose.connect(URI, {
     console.log('Connected to MongoDB')
 })
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'))
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+    })
+}
+
+
+
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
     console.log('Server is running on port', PORT)
