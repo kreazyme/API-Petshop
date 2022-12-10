c62358147242f869e276c9549b7516a419be98a3
Duc Thong <thongtd@dipro.vn>
2022-12-10 19:41:26 +0700

    feat: API get history order

--- controllers/order/orderCtrl.js
+++ controllers/order/orderCtrl.js
@@ -286,6 +286,35 @@ const orderCtrl = {
             })
         res.send({ message: "Order update not successfully" });
     },
+    getOrdersByTime: async (req, res) => {
+        try {
+            const { start, end } = req.body;
+            const userID = await authMe(req);
+            var endDate, startDate;
+            try {
+                endDate = new Date(end);
+                startDate = new Date(start);
+            }
+            catch (err) {
+                res.send({ message: "Wrong date format" });
+                return;
+            }
+            const orders = await Orders.find({
+                createdAt: { $gte: new Date(startDate), $lte: new Date(endDate) },
+                user_id: userID,
+                status: "Paid"
+            }, {
+                createdAt: 1,
+                total: 1,
+                status: 1,
+                listOrderItems: 1
+            });
+            res.send(orders);
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json({ msg: err.message });
+        }
+    },
 
 }
 
