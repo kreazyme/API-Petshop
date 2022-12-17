c2d43a163e0da2d8fa048d3bdf302fff89a1c192
Soukthavilay <Soukthavilay2000@gmail.com>
2022-12-17 20:29:27 +0700

    Fix router img & product check auth ,admin

--- routes/imgRouter.js
+++ routes/imgRouter.js
@@ -44,7 +44,7 @@ router.post('/upload', (req, res) =>{
 })
 
 // Delete image only admin can use
-router.post('/destroy',auth , authAdmin, (req, res) =>{
+router.post('/destroy', (req, res) =>{
     try {
         const {public_id} = req.body;
         if(!public_id) return res.status(400).json({msg: 'Không có hình ảnh nào được chọn'})
